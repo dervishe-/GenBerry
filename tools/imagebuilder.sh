b5fab4d2752d7cdf9d2a826e1061655e1edb257b
Alexandre Keledjian <dervishe@yahoo.fr>
2015-12-26 16:31:35 +0100

    Adding a fresh portage tree in the sdcard process

--- tools/imagebuilder.sh
+++ tools/imagebuilder.sh
@@ -26,7 +26,7 @@ LAYOUT="2048,65536,c\n67584,,83"
 . ./helpers.sh
 
 clear
-echo -e "$HSTAR Checking requirements (1/12): " #{{{
+echo -e "$HSTAR Checking requirements (1/13): " #{{{
 echo -en "\t$HSTAR Is ${DEVICE} exists ? "
 ls $DEVICE >> $LOG 2>&1
 BUFFER=$?
@@ -44,29 +44,29 @@ read rep
 [[ $rep =~ [Yy](es)? ]] || exit 1
 echo -e "\n"
 
-echo -en "$HSTAR Building working dir (2/12): " #{{{
+echo -en "$HSTAR Building working dir (2/13): " #{{{
 ([[ -d $WDIR ]] || mkdir "$WDIR") && cd "$WDIR" >> $LOG 2>&1
 printResult $?
 #}}}
 
-echo -en "$HSTAR Retrieving the fingerprint (3/12): " #{{{
+echo -en "$HSTAR Retrieving the fingerprint (3/13): " #{{{
 [[ -f $HASH ]] && rm $HASH
 getFile $LOCATION/$HASH
 #}}}
 
-echo -en "$HSTAR Retrieving stage 4 (4/12): " #{{{
+echo -en "$HSTAR Retrieving stage 4 (4/13): " #{{{
 retrieveFile $FILE $HASH $LOCATION
 #}}}
 
-echo -en "$HSTAR Checking the new file's fingerprint (5/12): " #{{{
+echo -en "$HSTAR Checking the new file's fingerprint (5/13): " #{{{
 checkFingerprint $HASH
 #}}}
 
-echo -en "$HSTAR Retrieving the file's signature (6/12): " #{{{
+echo -en "$HSTAR Retrieving the file's signature (6/13): " #{{{
 getFile $LOCATION/$SIG
 #}}}
 
-echo -en "$HSTAR Checking file's signature (7/12): " #{{{
+echo -en "$HSTAR Checking file's signature (7/13): " #{{{
 checkSignature $SIG $FILE
 if [[ $? -ne 0 ]]; then
 	echo -e "\tSignature's problem: the file don't seems to be legit..."
@@ -76,7 +76,7 @@ if [[ $? -ne 0 ]]; then
 fi
 #}}}
 
-echo -en "$HSTAR Partitionning the sdcard (8/12): " #{{{
+echo -en "$HSTAR Partitionning the sdcard (8/13): " #{{{
 echo -e $LAYOUT | sfdisk $DEVICE >> $LOG 2>&1
 partprobe $DEVICE >> $LOG 2>&1
 BUFFER=$?
@@ -84,7 +84,7 @@ printResult $BUFFER
 [[ $BUFFER -eq 0 ]] || exit 1
 #}}}
 
-echo -e "$HSTAR Formating the sdcard (9/12): " #{{{
+echo -e "$HSTAR Formating the sdcard (9/13): " #{{{
 echo -en "\t$HSTAR ${DEVICE}p1 in FAT16: "
 
 mkfs.vfat -F 16 ${DEVICE}p1 >> $LOG 2>&1
@@ -98,7 +98,7 @@ printResult $BUFFER
 [[ $BUFFER -eq 0 ]] || exit 1
 #}}}
 
-echo -e "$HSTAR Mounting partitions (10/12): " #{{{
+echo -e "$HSTAR Mounting partitions (10/13): " #{{{
 echo -en "\t$HSTAR root: "
 [[ -d $MOUNT_POINT ]] || mkdir $MOUNT_POINT >> $LOG 2>&1
 mount ${DEVICE}p2 $MOUNT_POINT
@@ -118,7 +118,7 @@ printResult $BUFFER
 [[ $BUFFER -eq 0 ]] || exit 1
 #}}}
 
-echo -en "$HSTAR Expanding the image on sdcard (11/12): " #{{{
+echo -en "$HSTAR Expanding the image on sdcard (11/13): " #{{{
 tar -xJpf $FILE -C $MOUNT_POINT >> $LOG 2>&1
 BUFFER=$?
 printResult $BUFFER
@@ -131,7 +131,14 @@ if [[ $BUFFER -ne 0 ]]; then
 fi
 #}}}
 
-echo -e "$HSTAR Cleaning all the stuffs (12/12): " #{{{
+echo -e "$HSTAR Creating portage tree on squashfs (12/13): " #{{{
+cd .. > /dev/null 2>&1
+${WDIR}/${MOUNT_POINT}/root/portage/portage2squa.sh
+mv ./portage.squashfs ${WDIR}/${MOUNT_POINT}/root/portage/ > /dev/null 2>&1
+cd - > /dev/null 2>&1
+#}}}
+
+echo -e "$HSTAR Cleaning all the stuffs (13/13): " #{{{
 echo -en "\t$HSTAR Syncing sdcard: "
 sync && printResult 0
 echo -en "\t$HSTAR Unmounting directory: "
--- tools/portage2squa.sh
+++ tools/portage2squa.sh
@@ -15,102 +15,102 @@ LOCATION=http://distfiles.gentoo.org/snapshots
 FILE=portage-latest.tar.xz
 SIG=${FILE}.gpgsig
 HASH=${FILE}.md5sum
-WDIR=./repository
+WDIR=./repository_alt
 TEST_DIR=./mnt
 SQUASH_FILE=portage.squashfs
 LOCAL_PORTAGE_DIR=portage
 #}}}
 . ./helpers.sh
 
-clear
-echo -e "$HSTAR Checking requirements (1/11): " #{{{
-echo -en "\t$HSTAR Is gpg installed ? "
+#clear
+echo -e "\t$HSTAR Checking requirements (1/11): " #{{{
+echo -en "\t\t$HSTAR Is gpg installed ? "
 checkGPG
-echo -en "\t$HSTAR Are you root ? "
+echo -en "\t\t$HSTAR Are you root ? "
 checkRoot
-echo -en "\t$HSTAR Are you connected ? "
+echo -en "\t\t$HSTAR Are you connected ? "
 checkConnectivity
 #}}}
 
-echo -en "$HSTAR Building working dir (2/11): " #{{{
+echo -en "\t$HSTAR Building working dir (2/11): " #{{{
 ([[ -d $WDIR ]] || mkdir "$WDIR") && cd "$WDIR" >> $LOG 2>&1
 printResult $?
 #}}}
 
-echo -en "$HSTAR Retrieving the fingerprint (3/11): " #{{{
+echo -en "\t$HSTAR Retrieving the fingerprint (3/11): " #{{{
 [[ -f $HASH ]] && rm $HASH
 getFile $LOCATION/$HASH
 #}}}
 
-echo -en "$HSTAR Retrieving the portage archive (4/11): " #{{{
+echo -en "\t$HSTAR Retrieving the portage archive (4/11): " #{{{
 retrieveFile $FILE $HASH $LOCATION
 #}}}
 
-echo -en "$HSTAR Checking file's fingerprint (5/11): " #{{{
+echo -en "\t$HSTAR Checking file's fingerprint (5/11): " #{{{
 checkFingerprint $HASH
 #}}}
 
-echo -en "$HSTAR Retrieving the file's signature (6/11): " #{{{
+echo -en "\t$HSTAR Retrieving the file's signature (6/11): " #{{{
 getFile $LOCATION/$SIG
 #}}}
 
-echo -en "$HSTAR Checking file's signature (7/11): " #{{{
+echo -en "\t$HSTAR Checking file's signature (7/11): " #{{{
 checkSignature $SIG $FILE
 if [[ $? -ne 0 ]]; then
-	echo -e "\tSignature's problem: the file don't seems to be legit..."
-	echo -e "\tPerhap's you don't have imported the Gentoo public key:"
-	echo -e "\thttps://www.gentoo.org/downloads/signatures/"
+	echo -e "\t\tSignature's problem: the file don't seems to be legit..."
+	echo -e "\t\tPerhap's you don't have imported the Gentoo public key:"
+	echo -e "\t\thttps://www.gentoo.org/downloads/signatures/"
 	exit 1;
 fi
 #}}}
 
-echo -en "$HSTAR Expanding the portage tree (8/11): " #{{{
+echo -en "\t$HSTAR Expanding the portage tree (8/11): " #{{{
 tar -xJpf $FILE >> $LOG 2>&1
 BUFFER=$?
 printResult $BUFFER
 if [[ $BUFFER -ne 0 ]]; then
-	echo -e "\tCheck the free space in your filesystem."
-	echo -e "\tDon't forget to erase the '$LOCAL_PORTAGE_DIR' directory before"
-	echo -e "\trunning this script again."
+	echo -e "\t\tCheck the free space in your filesystem."
+	echo -e "\t\tDon't forget to erase the '$LOCAL_PORTAGE_DIR' directory before"
+	echo -e "\t\trunning this script again."
 	exit 1;
 fi
 #}}}
 
-echo -en "$HSTAR Building the Portage Squash image (9/11): " #{{{
+echo -en "\t$HSTAR Building the Portage Squash image (9/11): " #{{{
 mksquashfs $LOCAL_PORTAGE_DIR $SQUASH_FILE >> $LOG 2>&1
 BUFFER=$?
 printResult $BUFFER
 [[ $BUFFER -ne 0 ]] && exit 1
 #}}}
 
-echo -en "$HSTAR Testing the new image (10/11): " #{{{
+echo -en "\t$HSTAR Testing the new image (10/11): " #{{{
 [[ -d $TEST_DIR ]] || mkdir $TEST_DIR >> $LOG 2>&1
 mount -o loop -t squashfs $SQUASH_FILE $TEST_DIR >> $LOG 2>&1
 BUFFER=$?
 printResult $BUFFER
 if [[ $BUFFER -ne 0 ]]; then
-	echo -e "\tUnable to mount the new squash image."
-	echo -e "\tCheck your kernel if it can handle them"
-	echo -e "\tOr if you have the authorization to mount squashfs volumes."
+	echo -e "\t\tUnable to mount the new squash image."
+	echo -e "\t\tCheck your kernel if it can handle them"
+	echo -e "\t\tOr if you have the authorization to mount squashfs volumes."
 	exit 1
 fi
 umount $TEST_DIR
 #}}}
 
-echo -en "$HSTAR Cleaning all the stuffs (11/11): " #{{{
+echo -en "\t$HSTAR Cleaning all the stuffs (11/11): " #{{{
 cd .. >> $LOG 2>&1
 mv $WDIR/$SQUASH_FILE . >> $LOG 2>&1
 BUFFER=$?
 if [[ $BUFFER -ne 0 ]]; then
 	printResult $BUFFER
-	echo -e "\tUnable to move the squashfs file: '$SQUASH_FILE' from the temporary directory"
+	echo -e "\t\tUnable to move the squashfs file: '$SQUASH_FILE' from the temporary directory"
 	exit 1
 fi
 rm -Rf $WDIR >> $LOG 2>&1
 BUFFER=$?
 printResult $BUFFER
 if [[ $BUFFER -ne 0 ]]; then
-	echo -e "\tUnable to clean and delete the temporary directory"
+	echo -e "\t\tUnable to clean and delete the temporary directory"
 	exit 1
 fi
 #}}}
